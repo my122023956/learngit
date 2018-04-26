1f8ebd08fa5d2c03e2d0fb419a952da21e97fcce
my122023956 <122023956@qq.com>
2018-04-26 16:31:34 +0800

    add mxmc 0.1

---
+++ mxmc/server/mxmc.php
@@ -0,0 +1,31 @@
+<?php
+	$mysqli = new mysqli("localhost","root","root","mydb");
+	$query = 'select * from t_mxmc';
+	$mysqli->query('SET NAMES UTF8');//解决中文乱码问题
+	$result = $mysqli->query($query);
+	if ($result) {
+		//循环输出结果集中的记录
+		while($row =$result->fetch_row() ){
+		//中文转码 
+			echo (encode_json($row)).',';
+		}
+	}else {
+		echo "查询失败";
+	}
+	
+	function encode_json($str) {
+		return urldecode(json_encode(url_encode($str)));
+	}
+	function url_encode($str) {
+		if(is_array($str)) {
+			foreach($str as $key=>$value) {
+				$str[urlencode($key)] = url_encode($value);
+			}
+		} else {
+			$str = urlencode($str);
+		}
+		return $str;
+	}
+	$result->free();
+	$mysqli->close();
+?>
